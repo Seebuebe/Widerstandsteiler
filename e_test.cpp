846fb160e0e120095efc4b26ad28fb6db4749180
Luca <luca-jost@hotmail.com>
2019-10-17 15:56:57 +0200

    Initial commit

---
+++ e_test.cpp
@@ -0,0 +1,26 @@
+/*
+ * e_test.cpp
+ *
+ *  Created on: Oct 10, 2019
+ *      Author: user
+ */
+
+#include <iostream>
+#include <math.h>
+#include "Ecalc.h"
+
+using namespace std;
+
+int main (void) {
+  Ecalc ecalc;
+  ecalc.calculate(5, 0.001, 2, 0);
+  cout << "Resistor Series E" << ecalc.getSeries() << endl;
+  cout << "Resistor 1: " << ecalc.getResistor1() << endl;
+  cout << "Resistor 2: " << ecalc.getResistor2() << endl;
+  cout << "Output Voltage: " << ecalc.getOutput() << endl;
+  cout << "Absolute Error: " << ecalc.getErrorABS() << endl;
+  cout << "Relative Error: " << ecalc.getErrorRel()*100 << "%" << endl;
+  return 0;
+}
+
+
